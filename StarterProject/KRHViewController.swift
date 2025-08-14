eb35a49f0296461f1f387b882b157d575d8e1b72
“RiHA” <riyaya0320@gmail.com>
2025-08-14 14:10:06 +0900

    feature: KRHViewController

---
+++ StarterProject/KRHViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StarterProject
+//
+//  Created by 김리하 on 8/14/25.
+//
+
+import UIKit
+
+class KRHViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 김리하 계정에서 만든 KRHViewController 입니다 !!
+        // Do any additional setup after loading the view.
+    }
+    
+}
